0b53dd1ac589b9761c6e720831d79a069a18570a
Jaedoo <dlwoen9@naver.com>
2022-03-21 23:49:08 +0900

    ip 수정

--- consumer/src/main/java/com/jd/listener/TestListener.java
+++ consumer/src/main/java/com/jd/listener/TestListener.java
@@ -1,6 +1,18 @@
 package com.jd.listener;
 
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.io.IOException;
+
 /**
  * @author Jaedoo Lee
  */
-public class TestListener {}
+
+public class TestListener {
+
+    @KafkaListener(topics = "test")
+    public void consume(String message) throws IOException {
+        System.out.printf("Consumed message : %s%n", message);
+    }
+
+}
