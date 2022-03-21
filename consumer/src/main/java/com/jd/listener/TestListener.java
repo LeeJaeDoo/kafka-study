7b07f9f7aab01bcc88b978c7dcc929f9552b12ae
Jaedoo <dlwoen9@naver.com>
2022-03-22 00:31:03 +0900

    consumer 연동

--- consumer/src/main/java/com/jd/listener/TestListener.java
+++ consumer/src/main/java/com/jd/listener/TestListener.java
@@ -1,16 +1,17 @@
 package com.jd.listener;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 /**
  * @author Jaedoo Lee
  */
-
+@Component
 public class TestListener {
 
-    @KafkaListener(topics = "test")
+    @KafkaListener(topics = "test", groupId = "foo")
     public void consume(String message) throws IOException {
         System.out.printf("Consumed message : %s%n", message);
     }
