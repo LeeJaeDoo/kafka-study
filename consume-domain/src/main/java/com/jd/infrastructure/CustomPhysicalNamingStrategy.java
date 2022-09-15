9d0bbf8c08bf334374a53d734c28df60eed359d3
Jaedoo <dlwoen9@naver.com>
2022-09-15 23:11:44 +0900

    table naming 전략 구현체 추가

---
+++ consume-domain/src/main/java/com/jd/infrastructure/CustomPhysicalNamingStrategy.java
@@ -0,0 +1,42 @@
+package com.jd.infrastructure;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+import java.util.Locale;
+
+/**
+ * @author Jaedoo Lee
+ */
+public class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    private Identifier convertToSnakeCase(Identifier identifier) {
+        return Identifier.toIdentifier(identifier.getText().replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase(Locale.ROOT));
+    }
+}
