128e302822f9f5b917719be32bf032fe63fe8cf2
Jaedoo <dlwoen9@naver.com>
2022-09-29 21:55:42 +0900

    JacksonUtils 수정

--- common/src/main/java/com/jd/config/JacksonUtils.java
+++ common/src/main/java/com/jd/config/JacksonUtils.java
@@ -1,5 +1,7 @@
 package com.jd.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,16 +11,20 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
+import org.apache.logging.log4j.util.Strings;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Jaedoo Lee
  */
 @UtilityClass
+@Slf4j
 public class JacksonUtils {
 
     private static ObjectMapper getObjectMapper() {
@@ -39,6 +45,34 @@ public class JacksonUtils {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         return objectMapper.readValue(target, tClass);
+    }
+
+    public static <T> T toModelOrNull(final String target, final Class<T> tClass) {
+        final ObjectMapper objectMapper = getObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        try {
+            return objectMapper.readValue(target, tClass);
+        } catch (JsonProcessingException e) {
+            log.error("parsing error", e);
+            return null;
+        }
+    }
+
+    public static String toJson(final Object object) {
+        if (object == null) {
+            return Strings.EMPTY;
+        }
+
+        final ObjectMapper objectMapper = getObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (final JsonProcessingException e) {
+            log.error("json convert error");
+            return Strings.EMPTY;
+        }
     }
 
 }
